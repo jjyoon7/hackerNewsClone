de9d10afa814f161c01644aed5b80f38306ca9f5
jjyoon7 <jay.yoon7@gmail.com>
2020-03-12 17:07:23 +0100

    format the data in Story component

--- components/Story.js
+++ components/Story.js
@@ -1,6 +1,25 @@
 export default function Story(story) {
     return `
         <div class="story">
-            <span>${story.index}</span>
+            <div>            
+                <span class="gray">${story.index}</span>
+                <span class="upvote">▲</span>
+                <a href="${story.url}">${story.title}</a>
+                <span>(${story.domain})</span>
+            </div>
+            <div>
+                <div class="gray">
+                    ${story.points} points by ${story.user} ${story.time_ago}
+                    |
+                    <a href="#/item?id=${story.id}">
+                      ${story.comments_count} comments
+                    </a>
+                    |
+                    <span class="favorite">
+                      <img class="heart" src="https://icon.now.sh/heart/ccc">
+                      Add To Favorites
+                    </span>
+                </div>
+            </div>
         </div>`
 }
