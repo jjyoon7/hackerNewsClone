f04564cc4cdb34adba529aaa05eb8de034c8e105
jjyoon7 <jay.yoon7@gmail.com>
2020-03-12 19:43:20 +0100

    Item function setup similiar to stories function

--- pages/item.js
+++ pages/item.js
@@ -1,5 +1,14 @@
 import view from "../utils/view.js"
+import Story from "../components/Story.js"
 
-export default function Item() {
-    view.innerHTML = `<div>item</div>`
+export default async function Item() {
+    const story = await getStory()
+    view.innerHTML = `<div>${Story(story)}</div>`
+}
+
+async function getStory() {
+    const storyId = window.location.hash.split("?id=")[1];
+    const response = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`)
+    const story = await response.json();
+    return story
 }
