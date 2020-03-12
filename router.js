d3eb3a4bd17770d9fb141934d46986751fa9eb0d
jjyoon7 <jay.yoon7@gmail.com>
2020-03-12 11:39:33 +0100

    adding a route for each router

--- router.js
+++ router.js
@@ -1,1 +1,23 @@
+import Stories from "./pages/stories"
+
 const router = new Navigator(null, true, "#")
+
+export default class RouterHandler {
+    constructor() {
+        this.createRoutes()
+    }
+
+    createRoutes() {
+        const routes = [
+            {path: "/", page: Stories}
+        ];
+
+        routes.forEach(route => {
+            //this syntax is for adding a route Navigo syntax
+            //router.on('path', callback).resolve()
+            router.onLine(route.path, () => {
+                console.log(route.page());
+            }).resolve();
+        })
+    }
+}
