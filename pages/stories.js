41e24df319aea3418b722798d2545062cf3614d0
jjyoon7 <jay.yoon7@gmail.com>
2020-03-12 15:43:14 +0100

    make sure to use 'async' and 'await'

--- pages/stories.js
+++ pages/stories.js
@@ -1,16 +1,17 @@
 import view from "../utils/view.js"
 
-export default function Stories(path) {
-    getStories(path)
+export default async function Stories(path) {
+    const stories = await getStories(path)
+    console.log(stories)
     view.innerHTML = `<div>${path}</div>`
 }
 
-function getStories(path) {
+async function getStories(path) {
     const isHomeRoute = path === "/";
     if(isHomeRoute) {
         path = "/news"
     }
-    const response = fetch(`https://node-hnapi.herokuapp.com${path}`)
-    const stories = response.json();
+    const response = await fetch(`https://node-hnapi.herokuapp.com${path}`)
+    const stories = await response.json();
     return stories
 }
