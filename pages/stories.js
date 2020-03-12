6d29b68f4ae9eccf85817967a93a96e179002cf8
jjyoon7 <jay.yoon7@gmail.com>
2020-03-12 15:42:20 +0100

    fetching HN news for homeRoute

--- pages/stories.js
+++ pages/stories.js
@@ -1,5 +1,16 @@
 import view from "../utils/view.js"
 
 export default function Stories(path) {
+    getStories(path)
     view.innerHTML = `<div>${path}</div>`
+}
+
+function getStories(path) {
+    const isHomeRoute = path === "/";
+    if(isHomeRoute) {
+        path = "/news"
+    }
+    const response = fetch(`https://node-hnapi.herokuapp.com${path}`)
+    const stories = response.json();
+    return stories
 }
