a4b135ffb7976b0d7584725efb66ae37afdafd0b
jjyoon7 <jay.yoon7@gmail.com>
2020-03-13 18:57:16 +0100

    create Comment component, and using recursion, where Comment function calls itself within its body to render nested comments

--- pages/item.js
+++ pages/item.js
@@ -1,5 +1,6 @@
 import view from "../utils/view.js"
 import Story from "../components/Story.js"
+import Comment from "../components/Comment.js"
 import baseUrl from "../utils/baseUrl.js"
 
 export default async function Item() {
@@ -24,7 +25,7 @@ export default async function Item() {
             ${Story(story)}
         </div>
         <hr/>
-        ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join("") : "No comments"}
+        ${hasComments ? story.comments.map(comment => Comment(comment)).join("") : "No comments"}
     `
 }
 
