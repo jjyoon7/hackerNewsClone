e9da29d28eafa4d383ff6e1328ab88d6be59f277
jjyoon7 <jay.yoon7@gmail.com>
2020-03-13 19:21:28 +0100

    for the 'favorite', and manage teh toggle state, you need a reducer in order to manage those states

---
+++ store.js
@@ -0,0 +1,8 @@
+function reducer(state, action) {
+    switch(state.type) {
+        case "ADD":
+            //change state to add
+        case "REMOVE":
+            //change state to remove    
+    }
+}
