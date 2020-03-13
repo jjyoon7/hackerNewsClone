7e24905eaa11ab0d92abf5d34f91d1b7bfffbc92
jjyoon7 <jay.yoon7@gmail.com>
2020-03-13 19:34:38 +0100

    make sure to understand the action, and its objects and how to use them within the reducer

--- store.js
+++ store.js
@@ -1,12 +1,21 @@
-function reducer(state, action) {
+const initialState = {
+    favorites: []
+}
+
+function reducer(state = initialState, action) {
     switch(action.type) {
         case "ADD_FAVORITE":
-            //change state to add
+            const addedFavorite = action.payload.favorite;
+            const favorites = [...state.favorite, addedFavorite];
+            return {favorites};
         case "REMOVE_FAVORITE":
             //change state to remove
+            const removedFavoriteId = action.payload.favorite.id;
+            const favorites = state.favorites.filter(favorite => favorite.id !== removedFavoriteId)
+            return {favorite};
         default:
             return state    
     }
 }
 
-const action = { type: "ADD_FAVORITE" }
+const action = { type: "ADD_FAVORITE", payload: { favorite: { title: "story1", id: 1 } } }
